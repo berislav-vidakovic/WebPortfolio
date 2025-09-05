4ab452f22985466a81587e52f39b5eea30ed133e
BarryTheAnalyst <125129931+berislav-vidakovic@users.noreply.github.com>
2025-09-05 15:20:08 +0200

    Add files via upload
    Updated URLs for Render deployment:
    export const API_BASE_URL = "https://hellow-backend-cs.onrender.com"; // Render
    export const WEB_SOCKET_URL = "wss://hellow-backend-cs.onrender.com/ws"; // Render

--- HelloWorld/frontend/src/config.js
+++ HelloWorld/frontend/src/config.js
@@ -1,3 +1,8 @@
 // config.js
-export const API_BASE_URL = "http://localhost:5143";
-export const WEB_SOCKET_URL = "ws://localhost:5143/ws";
+
+//export const API_BASE_URL = "http://localhost:5143"; // local
+//export const WEB_SOCKET_URL = "ws://localhost:5143/ws"; // local
+
+export const API_BASE_URL = "https://hellow-backend-cs.onrender.com"; // Render
+export const WEB_SOCKET_URL = "wss://hellow-backend-cs.onrender.com/ws"; // Render
+
