7c09a1a9c909938261a3a3c39c28ab8c466e8a2d
BarryTheAnalyst <125129931+berislav-vidakovic@users.noreply.github.com>
2025-09-11 14:58:21 +0200

    Add files via upload

--- TestAutomation/tests/TestChatApp.spec.js
+++ TestAutomation/tests/TestChatApp.spec.js
@@ -2,6 +2,7 @@
 import { test, expect } from '@playwright/test';
 
 test.only('get started link', async ({ page }) => {
+  console.log("Hello world from ChatApp TAS!");
   await page.goto('/');
 
   //await page.pause();
